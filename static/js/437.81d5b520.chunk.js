4bd7c1a62505d6a05377acddd887f4ff83152698
Fredy228 <105521962+Fredy228@users.noreply.github.com>
2023-05-01 09:06:24 +0000

    Deploying to gh-pages from @ Fredy228/solar-man@35fe78678f62eef84be7a09c5dea99554968a9a5 🚀

---
+++ static/js/437.81d5b520.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksolar_man=self.webpackChunksolar_man||[]).push([[437],{4437:function(n,e,u){u.r(e);e.default=function(){return"Store"}}}]);
+//# sourceMappingURL=437.81d5b520.chunk.js.map
