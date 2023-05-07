927a45a5a3e6024572221d526008f4d92e66c7c7
Fredy228 <smagrovich58@gmail.com>
2023-05-07 13:11:21 +0300

    Update ForEnterprises.jsx

--- src/pages/OurServices/ForEnterprises.jsx
+++ src/pages/OurServices/ForEnterprises.jsx
@@ -1,11 +1,10 @@
 import { Container } from 'pages/Common.styled';
-import { Section, BoxForm } from './OurServices.styled';
+import { Section } from './OurServices.styled';
 import { OptionEnterprises } from 'components/Option/OptionEnterprises';
 import { BenefitsEnterprises } from 'components/Benefits/BenefitsEnterprises';
 import { Areas } from 'components/Areas/Areas';
 import { ElementsStationEnterprises } from 'components/ElementsStation/ElementsStationEnterprise';
 import { StepsWorkEnterprises } from 'components/StepsWork/StepsWorkEnterprises';
-import { SendPhone } from 'components/SendPhone/SendPhone';
 
 const ForEnterprises = () => {
   return (
